deb676235fcd5332facc0c10e1b2725e9301f6cb
Bosswell <pappakaritv@gmail.com>
2018-12-30 21:30:42 +0100

    Added posibility to select randomized questions from database

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -19,32 +19,15 @@ class QuestionRepository extends ServiceEntityRepository
         parent::__construct($registry, Question::class);
     }
 
-    // /**
-    //  * @return Question[] Returns an array of Question objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function getRandomEntities($id, $count = 40)
     {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
+        return  $this->createQueryBuilder('q')
+            ->addSelect('RAND() as HIDDEN rand')
+			->where('q.qualification = ?1')
+            ->setParameter(1, $id)
+            ->addOrderBy('rand')
+            ->setMaxResults($count)
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Question
-    {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
