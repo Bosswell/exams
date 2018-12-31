dc539204b4cc3268b9a084a761541e8027dcd063
Bosswell <pappakaritv@gmail.com>
2018-12-31 19:47:47 +0100

    Fixed image uploading

--- src/Entity/Stage.php
+++ src/Entity/Stage.php
@@ -107,7 +107,7 @@ class Stage
         return $this->imageName;
     }
 
-    public function setImageName(string $imageName): self
+    public function setImageName(?string $imageName): self
     {
         $this->imageName = $imageName;
 
