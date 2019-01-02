ff1ef68018b98909a9e2dd11b6735199ea312541
Bosswell <pappakaritv@gmail.com>
2019-01-02 21:09:02 +0100

    Added stage generator service and giving it a global twig reach

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,37 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Cache\Simple\FilesystemCache;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use App\Service\StagesGenerator;
+
 
 class HomePageController extends AbstractController
 {
     /**
      * @Route("/", name="home_page")
      */
-    public function index()
+    public function index(StagesGenerator $stagesGenerator)
     {
-		$serializer = new Serializer(array(), array(new JsonEncoder()));
-        $cache = new FilesystemCache();
-
-        if (!$cache->has('homepage.stages')) {
-
-            $em = $this->getDoctrine()->getManager();
-            $sql = '
-                SELECT s.id, s.designation, s.image_name, count(q.qualification_id) as "qualification_quantity",
-                count(DISTINCT(q.qualification_id)) as "stages_quantity" FROM `stage` s LEFT JOIN `stage_qualification` 
-                s_q ON s_q.stage_id = s.id LEFT JOIN `question` q ON q.qualification_id = s_q.qualification_id GROUP BY s_q.stage_id
-            ';
-            
-            $stmt = $em->getConnection()->prepare($sql);
-            $stmt->execute();
-            $stages =  $stmt->fetchAll();
-
-            $cache->set('homepage.stages', $serializer->encode($stages, 'json'));
-        }
-
-        $stages = $serializer->decode($cache->get('homepage.stages'), 'json');
+		$stages = $stagesGenerator->getAll();
 
         return $this->render('home_page/index.html.twig', [
             'stages'   =>  $stages,
---
+++ src/Service/StagesGenerator.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Cache\Simple\FilesystemCache;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Stage;
+
+class StagesGenerator
+{
+    private $em;
+    private $serializer;
+    private $cache;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->serializer = new Serializer(array(), array(new JsonEncoder()));
+        $this->cache = new FilesystemCache();
+
+        $this->em = $em;
+    }
+
+    /*
+    * Pull out all with the number of all questions
+    */
+    public function getAll()
+    {
+        if (!$this->cache->has('stages.all')) {
+
+            $sql = '
+                SELECT s.id, s.designation, s.image_name, count(q.qualification_id) as "qualification_quantity",
+                count(DISTINCT(q.qualification_id)) as "stages_quantity" FROM `stage` s LEFT JOIN `stage_qualification` 
+                s_q ON s_q.stage_id = s.id LEFT JOIN `question` q ON q.qualification_id = s_q.qualification_id GROUP BY s_q.stage_id
+            ';
+            
+            $stmt = $this->em->getConnection()->prepare($sql);
+            $stmt->execute();
+            $stages =  $stmt->fetchAll();
+
+            $this->cache->set('stages.all', $this->serializer->encode($stages, 'json'));
+        }
+
+        return $this->serializer->decode($this->cache->get('stages.all'), 'json');
+    }
+}
