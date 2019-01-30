a10a36b2af8293f023123c976608e7ab65aaa113
Bosswell <pappakaritv@gmail.com>
2019-01-30 23:01:51 +0100

    Add active class to menu on scroll or click

--- public/js/creative.js
+++ public/js/creative.js
@@ -80,7 +80,46 @@
           $item.attr('data-expanded', 'true');
       }
       
-  })
+  });
+
+  
+  let navbarItem = $('.navbar .nav-item');
+  let examsPosition = $('#egzaminy').offset().top - 100;
+  let deadlinePosition = $('#terminy').offset().top - 100;
+  let contactPosition = $('#contact').offset().top - 100;
+
+  checkPosition();
+
+  function checkPosition() {
+    let topPosition = $(window).scrollTop();
+
+    if (contactPosition <= topPosition) {
+      navbarItem.eq(2).addClass('active');
+  
+      navbarItem.eq(0).removeClass('active');
+      navbarItem.eq(1).removeClass('active');
+  
+    } else if (deadlinePosition <= topPosition) {
+      navbarItem.eq(1).addClass('active');
+  
+      navbarItem.eq(0).removeClass('active');
+      navbarItem.eq(2).removeClass('active');
+  
+    } else if (examsPosition <= topPosition) {
+      navbarItem.eq(0).addClass('active');
+  
+      navbarItem.eq(2).removeClass('active');
+      navbarItem.eq(1).removeClass('active');
+    } else {
+      navbarItem.eq(0).removeClass('active');
+      navbarItem.eq(1).removeClass('active');
+      navbarItem.eq(2).removeClass('active');
+    }
+  }
+  
+  $($(window).scroll(function () {
+    checkPosition();
+  }))
 
 })(jQuery); // End of use strict
 
